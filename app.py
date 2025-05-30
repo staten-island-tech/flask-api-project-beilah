2b9e411faaca565aa31148cc38dad200a7c16ab1
Beila H <beilah@nycstudents.net>
2025-05-30 09:09:25 -0400

    s

--- app.py
+++ app.py
@@ -7,35 +7,31 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # Get artworks from the Art Institute of Chicago API
-    response = requests.get("https://api.artic.edu/api/v1/artworks?fields=id,title,image_id,date_start,artist_display,date_display,main_reference_numb&page=1&limit=50")
+    response = requests.get("https://api.artic.edu/api/v1/artworks?fields=id,title,image_id,date_start,artist_display,date_display,main_reference_numb&page=1&limit=25")
     
     if response.status_code == 200:
         data = response.json()
-        """for art in data.get('data', {}):
-            image_id = art.get('image_id')
-            title = art.get('title')
-            artist = art.get('artist_title')"""
-        """artworks_list = data.get('data', [])  # Use 'data' key based on API structure
+        artworks_list = data.get('data', [])
     else:
-        artworks_list = []"""
-
-    """if image_id:
-        image_url = f"https://www.artic.edu/iiif/2/{image_id}/full/843,/0/default.jpg"
-    else:
-        image_url = None"""
-
-    artworks_list = data['data']
+        artworks_list = []
 
     return render_template("index.html", artworks=artworks_list)
-@app.route("/artworks/<int:id>")
-def artworkDetail():
-    response = requests.get(f"https://api.artic.edu/api/v1/artworks/{id}")
-    data = response.json()
-    artworkData = data.get("data", {})
-    imgId = artworkData.get("image_id")
-    url = f"https://www.artic.edu/iiif/2/{imgId}/full/843,/0/default.jpg"
-    render_template("/", artwork = {
-        'url': url
-    })
+
+# route for atrwork detail page
+@app.route('/artwork/<int:id>')
+def artworkDetail(id):
+    response = requests.get(f"https://api.artic.edu/api/v1/artworks?fields=id,title,image_id,date_start,artist_display,date_display,main_reference_numb&page=1&limit=25/{id}")
+
+    if response.status_code == 200:
+        data = response.json()
+        artworkData = data.get("data", {})
+        imgId = artworkData.get("image_id")
+        url = f"https://www.artic.edu/iiif/2/{imgId}/full/843,/0/default.jpg" if imgId else None
+    else:
+        artworkData = {}
+        url = None
+
+    return render_template("artworks.html", artwork=artworkData, image_url=url)
+    
 if __name__ == "__main__":
     app.run(debug=True)
